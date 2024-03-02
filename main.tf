78cac6442fe6359e7fe996562615700d7f3baedc
Baki Tuncer <98457585+batuncer@users.noreply.github.com>
2024-03-02 22:26:53 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,113 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-west-2"
+  region = var.region
 }
 
-resource "aws_s3_bucket" "hotelmainterraform" {
-  bucket = "hotelmainterraform"
+resource "aws_s3_bucket" "hotel" {
+  bucket = var.bucket_name
+
+
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = "prod"
+  }
 }
 
-resource "aws_s3_bucket_policy" "hotelmainterraform_policy" {
-  bucket = aws_s3_bucket.hotelmainterraform.id
+resource "aws_s3_bucket_acl" "hotel_acl" {
+  bucket = aws_s3_bucket.hotel.id
+  acl    = "public-read"
+}
 
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
+resource "aws_s3_bucket_cors_configuration" "hotel-cors_rules" {
+  bucket = aws_s3_bucket.hotel.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.hotel.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+  depends_on = [aws_s3_bucket_public_access_block.example]
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.hotel.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "hosting_bucket_policy" {
+  bucket = aws_s3_bucket.hotel.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
       {
-          "Sid": "PublicReadGetObject",
-          "Effect": "Allow",
-          "Principal": "*",
-          "Action": "s3:GetObject",
-          "Resource": "arn:aws:s3:::hotelmainterraform/*"
-      }
-  ]
+        Principal = "*"
+        Action = [
+          "s3:*",
+        ]
+        Effect = "Allow"
+        Resource = [
+          "arn:aws:s3:::${var.bucket_name}",
+          "arn:aws:s3:::${var.bucket_name}/*"
+        ]
+      },
+      {
+        Sid       = "PublicReadGetObject"
+        Principal = "*"
+        Action    = [
+          "s3:GetObject",
+        ]
+        Effect    = "Allow"
+        Resource  = [
+          "arn:aws:s3:::${var.bucket_name}",
+          "arn:aws:s3:::${var.bucket_name}/*"
+        ]
+      },
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.example]
 }
-POLICY
+
+resource "aws_s3_bucket_website_configuration" "hosting_bucket_website_configuration" {
+  bucket = aws_s3_bucket.hotel.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_object" "source_files" {
+    bucket = aws_s3_bucket.hotel.id
+
+    for_each = fileset("../build", "**/*")
+
+    key          = each.value
+    source       = "../build/${each.value}"
 }
